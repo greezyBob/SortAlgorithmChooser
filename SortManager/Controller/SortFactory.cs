ac2d04f31b60f3886eae92573530a8cf47076419
Jai Kothari <jai25@live.co.uk>
2022-10-07 14:37:05 +0100

    added from dev branch.

--- SortManager/Controller/SortFactory.cs
+++ SortManager/Controller/SortFactory.cs
@@ -4,37 +4,37 @@
 //using System.Text;
 ////using Model;
 
-//namespace Controller
-//{
-//    public class SortFactory
-//    {
-//        public int[] ChooseSorter(int input, int[] arr)
-//        {
-//            int[] result = new int[arr.Length];
-//            if (input == 1)
-//            {
-//               var sorter = new BubbleSortModel();
-//               result = sorter.Sort(arr);
-//            }
-//            else if (input == 2)
-//            {
-//                var sorter = new MergeSortModel();
-//                result =  sorter.Sort(arr);
-//            }
-//            else if (input == 3)
-//            {
-//                var sorter = new DotNetSort();
-//                result = sorter.Sort(arr);
-//            }
-//            else if (input == 4)
-//            {
-//                //exit
-//            }
-//            else
-//            {
-//              //input invalid      
-//            }
-//            return result;
-//        }
-//    }
-//}
+namespace Controller
+{
+    public class SortFactory
+    {
+        public int[] ChooseSorter(int input, int[] arr)
+        {
+            int[] result = new int[arr.Length];
+            if (input == 1)
+            {
+               var sorter = new BubbleSortModel();
+               result = sorter.Sort(arr);
+            }
+            else if (input == 2)
+            {
+                var sorter = new _model();
+                result =  sorter.Sort(arr);
+            }
+            else if (input == 3)
+            {
+                var sorter = new DotNetSort();
+                result = sorter.Sort(arr);
+            }
+            else if (input == 4)
+            {
+                //exit
+            }
+            else
+            {
+              //input invalid      
+            }
+            return result;
+        }
+    }
+}
