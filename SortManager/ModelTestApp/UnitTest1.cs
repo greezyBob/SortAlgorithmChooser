a4f6d47decb7e6e11801910c6dfc5e4cdcebf3d7
Rob <robertma.green@gmail.com>
2022-10-07 10:37:29 +0100

    project setup

---
+++ SortManager/ModelTestApp/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ModelTestApp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
