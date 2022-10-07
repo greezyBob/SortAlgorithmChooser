9947ac81fefc2b4070a817a93b3fdd0fd8e30ace
Jai Kothari <jai25@live.co.uk>
2022-10-07 14:27:32 +0100

    Added CoLoUr.

--- SortManager/Controller/SortFactory.cs
+++ SortManager/Controller/SortFactory.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Model;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+////using Model;
 
-namespace Controller
-{
-    public class SortFactory
-    {
-        public int[] ChooseSorter(int input, int[] arr)
-        {
-            int[] result = new int[arr.Length];
-            if (input == 1)
-            {
-               var sorter = new BubbleSortModel();
-               result = sorter.Sort(arr);
-            }
-            else if (input == 2)
-            {
-                var sorter = new MergeSortModel();
-                result =  sorter.Sort(arr);
-            }
-            else if (input == 3)
-            {
-                var sorter = new DotNetSort();
-                result = sorter.Sort(arr);
-            }
-            else if (input == 4)
-            {
-                //exit
-            }
-            else
-            {
-              //input invalid      
-            }
-            return result;
-        }
-    }
-}
+//namespace Controller
+//{
+//    public class SortFactory
+//    {
+//        public int[] ChooseSorter(int input, int[] arr)
+//        {
+//            int[] result = new int[arr.Length];
+//            if (input == 1)
+//            {
+//               var sorter = new BubbleSortModel();
+//               result = sorter.Sort(arr);
+//            }
+//            else if (input == 2)
+//            {
+//                var sorter = new MergeSortModel();
+//                result =  sorter.Sort(arr);
+//            }
+//            else if (input == 3)
+//            {
+//                var sorter = new DotNetSort();
+//                result = sorter.Sort(arr);
+//            }
+//            else if (input == 4)
+//            {
+//                //exit
+//            }
+//            else
+//            {
+//              //input invalid      
+//            }
+//            return result;
+//        }
+//    }
+//}
